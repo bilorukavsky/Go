f62a4326fe42da9a73c3f3db092d086bae9bcdf8
bilorukavsky <90616723+bilorukavsky@users.noreply.github.com>
2023-05-17 21:27:32 +0300

    Add files via upload

---
+++ counting_sheep.go
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/54edbc7200b811e956000556/go
+
+package main
+
+func CountSheeps(numbers []bool) int {
+	var sum int = 0
+	for _, elem := range numbers {
+		if elem {
+			sum++
+		}
+	}
+	return sum
+}
+
+func main{}
