068ddbb3609ff04108b66f999eb025d43fb0c725
bilorukavsky <90616723+bilorukavsky@users.noreply.github.com>
2023-05-17 21:37:24 +0300

    Add files via upload

---
+++ century_from_year.go
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+
+package main
+
+func century(year int) int {
+	if year % 100 == 0{
+	  return year / 100
+	}
+   return year / 100+1
+}
+
+func main{}
---
+++ even_or_odd.go
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+package main
+
+func EvenOrOdd(number int) string {
+	if number%2 == 0 {
+		return "Even"
+	}
+	return "Odd"
+}
+
+func main() {}
