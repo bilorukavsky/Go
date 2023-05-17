ece152ee63d15fabe8e49260188a2b40df17dece
bilorukavsky <90616723+bilorukavsky@users.noreply.github.com>
2023-05-17 21:30:43 +0300

    Add files via upload

---
+++ square_sum.go
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/515e271a311df0350d00000f
+
+package main
+
+func SquareSum(numbers []int) int {
+	var summ = 0
+	  for i := 0; i < len(numbers); i++{
+		summ += numbers[i] * numbers[i]
+	  }
+	return summ
+}
+
+func main{}
